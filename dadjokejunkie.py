fe7702fd34f93d03f6a42182f1ccc3e1b766ffdb
Chase DeHan <chasedehan@yahoo.com>
2018-04-05 22:17:13 -0600

    Front End works correctly

--- dadjokejunkie.py
+++ dadjokejunkie.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-from send_jokes import daily_fix, sign_up
+from send_jokes import sign_up
+from config import Defaults
 
 app = Flask(__name__)
 
@@ -7,16 +8,18 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        print(request.form)
-        # TODO: check phone number against the DB to see if it is already in use
-        sign_up(request.form['phone_number'])
-        return render_template('thanks.html')
+        status = sign_up(request.form['phone_number'])
+        if status == "success":
+            return render_template('thanks.html')
+        elif status == "stop":
+            return render_template('stop.html')
+        elif status == "registered":
+            return render_template('registered.html')
+        elif status == "bad":
+            return render_template('bad.html')
 
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    # TODO: set process in here to automatically kick off at a particular time
-    # Or this could be a separate application
-    daily_fix()
-    app.run(port=31000)
+    app.run(port=Defaults.port)
--- send_jokes.py
+++ send_jokes.py
@@ -1,44 +1,55 @@
 # send_jokes.py
+# TODO: create some logs so I can see what is happening
 from twilio.rest import Client
+from datetime import datetime
+import MySQLdb
+from config import SMS, DB
 
-account_sid = "ACdd403c2cdf304cdfb3e00eae998b8a59"
-auth_token = "39e9082f27f913a4d6107ce75187aeee"
+account_sid = SMS.account_sid
+auth_token = SMS.auth_token
 
 client = Client(account_sid, auth_token)
+db = MySQLdb.connect(host=DB.host,
+                     port=DB.port,
+                     user=DB.user,
+                     passwd=DB.password,
+                     db=DB.db)
+db.autocommit(True)
+
 
 def sign_up(phone_number):
-    # TODO: check number against the DB to make sure it is ok
+    crsr = db.cursor()
+    exists = crsr.execute("""select Stop from Users WHERE PhoneNumber = %s LIMIT 1""", (phone_number,))
+    crsr.close()
+    if exists:
+        crsr = db.cursor()
+        crsr.execute("""select Stop from Users WHERE PhoneNumber = %s""", (phone_number,))
+        stop = crsr.fetchall()[0][0]
+        crsr.close()
+        if stop:
+            return "stop"
+        return "registered"
+
     # Then, send the first message to the customer
     message = """Welcome to DadJokeJunkie, we will get you started right now:
     
     What do you call a fake noodle? An Impasta!
                 
     Reply with STOP at any time to opt out."""
-    # TODO: need to validate the number first, to make sure it works. Twilio is 1 sec per number
-    # Do I set up multiple phone numbers for higher throughput? Probably, but that adds a little cost
+
     try:
         client.api.account.messages.create(
             to=phone_number,
-            from_="+18019358742",
+            from_=SMS.phone_one,
             body=message)
-        return True
     except:
-        return False
-    # TODO: write that phone number out to the DB
+        return "bad"
 
-def daily_fix():
-    # TODO: build out the daily process that will actually occur
-    # Look up the users where Stop=0
-    #
-    # Then, look up the joke of the day - random number generator off length of the table
-        # I'm thinking to pull down the entire list and randomly select for a given subset of users
-        # So that people get different jokes - at least a different joke per phone number
-
-    # Loop through each of the users, deliver their joke
-
-    # Maybe multithread it according to the phone numbers so they run in parallel.
-
-
-    # Need to generate webhook to make sure that when STOP, they flip the Stop flag in the DB
-    return True
+    crsr = db.cursor()
+    crsr.execute("""INSERT INTO Users
+                  (PhoneNumber, Stop, CreatedDate)
+                  VALUES (%s, 0, %s);""",
+                 (phone_number, str(datetime.now())))
+    crsr.close()
+    return "success"
 
