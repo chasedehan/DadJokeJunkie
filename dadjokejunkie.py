4c28292b15d3da6af786fb5422687b1af31baada
Chase DeHan <chasedehan@yahoo.com>
2018-04-23 22:18:08 -0600

    added error pages

--- dadjokejunkie.py
+++ dadjokejunkie.py
@@ -50,14 +50,22 @@ def index():
     return render_template('index.html')
 
 
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
+# @app.errorhandler(500)
+# def server_error(e):
+#     logging.exception('An error occurred during a request.')
+#     return """
+#     An internal error occurred: <pre>{}</pre>
+#     See logs for full stacktrace.
+#     """.format(e), 500
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(port=Defaults.port)
